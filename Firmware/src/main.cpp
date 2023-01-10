6e0937ea003483a83d3be6935746b1365d5057ec
KenwoodFox <kenwood364@gmail.com>
2023-01-10 13:38:01 -0500

    Modify boilerplate for vtol

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -1,24 +1,14 @@
 /**
  * @file main.cpp
  * @author Joe
- * @brief Source code for EG-310 Pendulum demonstrator
+ * @brief Source code for VTOL Board
  */
-
-//  This option forces Encoder.h to use actual assembly code to squeeze extra performance from the system
-// #define ENCODER_OPTIMIZE_INTERRUPTS
 
 // Libs
 #include <Arduino.h>
-// #include <Encoder.h>
-#include <TFT.h>
-#include <SPI.h>
 
 // Headers
 #include "boardPins.h"
-
-// Instantiate objects
-// Encoder pendulumEncoder(ENC_A, ENC_B);
-TFT disp = TFT(TFT_CS, TFT_A0, TFT_RESET);
 
 void setup()
 {
@@ -28,30 +18,10 @@ void setup()
     Serial.println(MOTD);
 
     // // Configure Pins
-    // pinMode(STAT_LED, OUTPUT);
-    // pinMode(ENABLE, OUTPUT);
-    // pinMode(STEP, OUTPUT);
-    // pinMode(DIR, OUTPUT);
-    // pinMode(TFT_RESET, OUTPUT);
-    // pinMode(TFT_CS, OUTPUT);
-    // pinMode(TFT_A0, OUTPUT);
-
-    // pinMode(PPIN, INPUT);
-    // pinMode(IPIN, INPUT);
-    // pinMode(DPIN, INPUT);
-    // pinMode(STOP_MODE, INPUT);
-    // pinMode(START, INPUT);
-
-    // Configure TFT
-    disp.begin();
+    pinMode(STAT_LED, OUTPUT);
 }
 
 void loop()
 {
-    disp.background(255, 0, 0);
-    Serial.println("Set bkground to red");
-    delay(500);
-    disp.background(0, 255, 0);
-    Serial.println("Set bkground to green");
     delay(500);
 }
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # VTOL-Board
-Just the board and firmware for the EG-310 VTOL project
+
+The board and firmware for the EG-310 VTOL project.
+
+
+## Instructions to include this in your own project repo
+
+I recommend using git submodules, add this to your tree like this
+
+```shell
+cd my_project
+git submodule add https://github.com/KenwoodFox/VTOL-Board
+git add -A
+git commit -m "Added VTOL board
+```
